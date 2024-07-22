1ac840b6539da158754e897f6dc70c5c0b062a75
Vovarama1992 <vovayhh9988@gmail.com>
2024-07-22 07:06:54 +0300

    no_trailing

--- api/axiosClient.ts
+++ api/axiosClient.ts
@@ -1,13 +1,8 @@
 import axios from 'axios';
 
-// Функция для добавления слэша к URL, если его нет
-const addTrailingSlash = (url: string) => {
-  return url.endsWith('/') ? url : `${url}/`;
-};
-
 export const api = axios.create({
   withCredentials: true,
-  baseURL: addTrailingSlash('https://api.moon-gamble.fans'), // проксирование запросов через Next.js
+  baseURL: 'https://api.moon-gamble.fans/', // проксирование запросов через Next.js
   headers: {
     'Content-Type': 'application/json',
   },
@@ -17,22 +12,11 @@ const token =
   typeof window !== 'undefined' && localStorage.getItem('accessToken');
 
 export const protectedApi = axios.create({
-  baseURL: addTrailingSlash('https://api.moon-gamble.fans'),
+  baseURL: 'https://api.moon-gamble.fans/',
   headers: {
     Authorization: token ? `Bearer ${token}` : '',
   },
 });
-
-// Пример использования обертки для добавления слэша к URL в запросах
-export const fetchUserData = async () => {
-  try {
-    const response = await protectedApi.get(addTrailingSlash('/users/me'));
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-    throw error;
-  }
-};
 
 /*import axios from 'axios';
 
