10454d6c7e3f13a48fa8b4d454652a94d1a85d8e
Vovarama1992 <vovayhh9988@gmail.com>
2024-08-12 11:20:46 +0300

    profile

--- components/sections/Banners/index.tsx
+++ components/sections/Banners/index.tsx
@@ -10,10 +10,18 @@ import NavigationArrow from './icons/NavigationArrow';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
 import { openWalletModal } from '@/context/modals';
 import { getUserData } from '@/api/user';
+import { toast } from 'react-toastify'; // Импорт для тостов
+import { useRouter } from 'next/navigation';
+import { linkVKFx } from '@/api/bonus'; // Убедитесь, что этот импорт корректен
+import { $user, setUser } from '@/context/user'; // Импорт Effector контекста
+import { useUnit } from 'effector-react'; // Импорт Effector хука
 
 export default function Banners() {
   const swiper = useSwiper();
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const user = useUnit($user);
+  const isLogin = !!user?.id;
+  const router = useRouter();
 
   useEffect(() => {
     const checkUserAuthorization = async () => {
@@ -37,6 +45,40 @@ export default function Banners() {
 
     checkUserAuthorization();
   }, []);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    console.log('VK Authorization Code:', code);
+
+    const processVKCode = async () => {
+      if (code) {
+        try {
+          const response = await linkVKFx(code); // Получаем объект пользователя с VK ID
+          if (user && response && response.vk_id) {
+            // Обновляем пользователя с новым vk_id
+            setUser({
+              ...user,
+              vk_id: response.vk_id,
+            });
+            toast.success('VK успешно привязан!');
+            // Очищаем URL от параметров
+            window.history.replaceState(null, '', window.location.pathname);
+          } else {
+            throw new Error('VK привязка не удалась');
+          }
+        } catch (error) {
+          console.error('Не удалось привязать VK:', error);
+          toast.error('Не удалось привязать VK. Попробуйте снова позже.');
+          router.push('/'); // Перенаправление на главную при ошибке
+        }
+      } else if (!isLogin) {
+        router.push('/'); // Перенаправление на главную, если пользователь не залогинен и нет кода
+      }
+    };
+
+    processVKCode();
+  }, [isLogin, router, user]);
 
   return (
     <section style={{ position: 'relative' }}>
