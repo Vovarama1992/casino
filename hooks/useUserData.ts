80dfb908bc0f5e79885f3045e7f47cceaec64078
Vovarama1992 <vovayhh9988@gmail.com>
2024-11-27 08:05:58 +0300

    big_hope

--- hooks/useUserData.ts
+++ hooks/useUserData.ts
@@ -1,19 +1,8 @@
 import { getUserBalance, getUserData } from '@/api/user';
 import { useUnit } from 'effector-react';
 import { $user, setUser } from '@/context/user';
-import { createBonusDeposit } from '@/api/wallet';
+
 import { useEffect } from 'react';
-
-// Функция для получения кешированных данных
-const getCachedUserData = () => {
-  const cachedUser = localStorage.getItem('user_data');
-  return cachedUser ? JSON.parse(cachedUser) : null;
-};
-
-// Функция для кеширования данных пользователя
-const setCachedUserData = (data: any) => {
-  localStorage.setItem('user_data', JSON.stringify(data));
-};
 
 export const useUserData = () => {
   const user = useUnit($user);
@@ -26,15 +15,6 @@ export const useUserData = () => {
         console.log('User data:', data);
 
         // Проверяем, если в новых данных есть vk_id, а в кешированных нет
-        const cachedUser = getCachedUserData();
-        if (data?.vk_id && cachedUser && !cachedUser?.vk_id) {
-          // Если vk_id появился, делаем бонусный депозит
-          await createBonusDeposit({
-            url: '/wallet/bonus-deposit',
-            paymentSystem: 'internal', // Укажите систему платежей
-            amount: 10, // Сумма бонуса
-          });
-        }
 
         console.log('Fetching user balance...');
         const { balance, bonus_balance, pure_balance } = await getUserBalance({
@@ -52,7 +32,7 @@ export const useUserData = () => {
         };
 
         setUser(newUserData);
-        setCachedUserData(newUserData); // Кешируем обновленные данные
+
         console.log('User data and balance set in context and cached.');
       } catch (error) {
         console.error('Error fetching user data or balance:', error);
